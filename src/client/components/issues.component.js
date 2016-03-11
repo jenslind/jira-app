ed66abd0592faf5a2deec86df18f677c1dfdaa20
Jens Lind <jens.lind@oddhill.se>
2016-03-11 17:44:02 +0100

    Link to full issue

--- src/client/components/issues.component.js
+++ src/client/components/issues.component.js
@@ -1,4 +1,5 @@
 import { Component, View } from 'angular2/core'
+import { ROUTER_DIRECTIVES } from 'angular2/router'
 import JiraService from '../services/jira.service'
 import '../scss/modules/_issues'
 
@@ -6,6 +7,7 @@ import '../scss/modules/_issues'
   selector: 'issues'
 })
 @View({
+  directives: [ROUTER_DIRECTIVES],
   template: require('../templates/issues.template')
 })
 export default class IssuesComponent {
