bf2a3feeedb8018b6b1be764784fa9286540f666
Jens Lind <jens.lind@oddhill.se>
2016-03-11 18:35:54 +0100

    openIssues -> issues

--- src/client/components/issues.component.js
+++ src/client/components/issues.component.js
@@ -17,6 +17,6 @@ export default class IssuesComponent {
   }
 
   ngOnInit() {
-    this.jira.openIssues$.subscribe(issues => this.issues = issues)
+    this.jira.issues$.subscribe(issues => this.issues = issues)
   }
 }
