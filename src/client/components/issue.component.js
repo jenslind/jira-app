6508099cbc8c12d4fe9c66c93219c5a520090b53
Jens Lind <jens.lind@oddhill.se>
2016-03-13 12:11:59 +0100

    Assign form

--- src/client/components/issue.component.js
+++ src/client/components/issue.component.js
@@ -1,19 +1,35 @@
 import { Component, View } from 'angular2/core'
+import { FormBuilder, Validators } from 'angular2/common'
 import { RouteParams } from 'angular2/router'
 import JiraService from '../services/jira.service'
+import Suggest from './suggest.component'
 
 @Component({
   selector: 'issue'
 })
 @View({
+  directives: [Suggest],
   template: require('../templates/issue.template')
 })
 export default class IssueComponent {
-  constructor(jira: JiraService, routeParams: RouteParams) {
+  constructor(jira: JiraService, routeParams: RouteParams, fb: FormBuilder) {
     this.jira = jira
     this.params = routeParams.params
     this.issue = {}
     this.assignable = []
+    this.fb = fb
+
+    this.assignForm = this.fb.group({
+      assigned: ['', Validators.required]
+    })
+  }
+
+  getAssignable() {
+    const users = []
+    for (let i = 0, len = this.assignable.length; i < len; i++) {
+      users.push(this.assignable[i].name)
+    }
+    return users
   }
 
   ngOnInit() {
