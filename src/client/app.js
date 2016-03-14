c94bdfa4d36501c41c1d46eb2e26dc4ea05d71ab
Jens Lind <jens.lind@oddhill.se>
2016-03-14 20:50:08 +0100

    Improved frame header

--- src/client/app.js
+++ src/client/app.js
@@ -8,6 +8,7 @@ import JiraService from './services/jira.service'
 import './scss/base/_base'
 import './scss/layout/_frame'
 import './scss/elements/_logo'
+import './scss/components/_btn'
 
 @Component({
   selector: 'jira-app'
