55cf0a4e3a514a5ebda2eab1f37dae74cfb6bc5f
Jens Lind <jens.lind@oddhill.se>
2016-03-15 09:40:17 +0100

    Save auth info

--- src/lib/Auth.js
+++ src/lib/Auth.js
@@ -1,15 +1,27 @@
+'use strict'
+
+const keytar = require('keytar')
+const storage = require('electron-json-storage')
+
 export default Auth {
 
-  static auth() {
-
-  }
-
-  static unAuth() {
-
+  static auth(info) {
+    storage.set('authSettings', {
+      baseUrl: info.baseUrl,
+      user: info.user
+    }, () => {
+      keytar.addPassword('Minira', info.user, info.pass)
+    })
   }
 
   static getAuth() {
-    
+    const settings = storage.get('authSettings')
+    const pass = keytar.getPassword('Minira', settings.user)
+    return {
+      baseUrl: settings.baseUrl,
+      user: settings.user,
+      pass: pass
+    }
   }
 
 }
